aae17db0c6ccc5c99b327fe02a2c3fc04d2deb0c
samuel-c-johnson <samuel.c.johnson2@gmail.com>
2018-02-02 12:50:01 +0000

    Passes tests for board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,1 +1,8 @@
+describe GameBoard do
 
+  let(:gameboard) {GameBoard.new}
+
+  it "initializes with 9 squares" do
+    expect(gameboard.board.length).to eq(9)
+  end
+end
--- spec/square_spec.rb
+++ spec/square_spec.rb
@@ -2,6 +2,6 @@ describe Square do
 
   it "is initialzed with an empty value" do
     square = Square.new
-    expect(square.value).to eq("")
+    expect(square.value).to eq(" ")
   end
 end
